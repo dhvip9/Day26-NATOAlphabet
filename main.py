0565948da3e0054c5fde21598ef4245ea007db50
dhvippatel <dhvipdp@gmail.com>
2021-12-22 15:27:28 +0530

    error exception lear

--- main.py
+++ main.py
@@ -27,5 +27,17 @@ df_nato_alphabet = pandas.DataFrame(nato_alphabet)
 dir_nato_alphabet = {row.letter: row.code for (index, row) in df_nato_alphabet.iterrows()}
 
 # TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-phonetic_lis = [dir_nato_alphabet[letter] for letter in input("Write Your Word\n>>").upper()]
-print(phonetic_lis)
+
+
+def phonetic_word():
+    word = input("Write Your Word\n>> ").upper()
+    try:
+        phonetic_lis = [dir_nato_alphabet[letter] for letter in word]
+    except KeyError:
+        print("Sorry only letter in Alphabet")
+        phonetic_word()
+    else:
+        print(phonetic_lis)
+
+
+phonetic_word()
